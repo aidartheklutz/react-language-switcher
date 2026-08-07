d428715e056b382413ecfb44066fb538748a49f8
aidar yrysov <aid4r@proton.me>
2026-08-07 23:04:27 +0600

    Create languageSwitcherComponent.jsx

---
+++ languageSwitcherComponent.jsx
@@ -0,0 +1,16 @@
+function LanguageSwitcher({ languages, language, onChange }) {
+  return (
+    <select
+      value={language}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {languages.map((lang) => (
+        <option key={lang.code} value={lang.code}>
+          {lang.name}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default LanguageSwitcher;
